58b891e66ca6ff98c664e329888a14fc4405a42f
Dsyleixc <102453885+Dsyleixc@users.noreply.github.com>
2025-03-20 12:08:38 -0400

    Added functionality to reset form on submit

--- src/form-intake.js
+++ src/form-intake.js
@@ -1,13 +1,14 @@
 'use strict';
-
+const form = document.querySelector('form');
 const createButton = document.querySelector('.form-button');
 let listIcon;
 let listName;
 
 createButton.addEventListener('click', function (e) {
     e.preventDefault();
-    listIcon = document.querySelector('#list-icon');
-    listName = document.querySelector('#list-name');
+    listIcon = document.querySelector('#list-icon').value;
+    listName = document.querySelector('#list-name').value;
+    form.reset();
 });
 
 export { listIcon, listName };
