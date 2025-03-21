29511dfc397cb1ab0c99cf873b763c7e06177580
Dsyleixc <102453885+Dsyleixc@users.noreply.github.com>
2025-03-20 21:59:35 -0400

    nav list click functionality fully completed

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './style.css';
 import { setupFormVisibility } from './form-functionality/form-render';
 import './form-functionality/form-submit';
 import { renderNavList } from './nav-list-render';
+import './nav-list-clicked';
 
 // init form functionality
 setupFormVisibility();
 
 // init nav list
 renderNavList();
+
+// handle new clicked list
--- src/nav-list-clicked.js
+++ src/nav-list-clicked.js
@@ -1,1 +1,31 @@
 'use strict';
+
+(function () {
+    const navList = document.querySelector('.nav-list');
+    let previousClickedList;
+    let newClickedList;
+
+    const removeClickedClass = function (list) {
+        if (previousClickedList) {
+            previousClickedList.classList.remove('clicked');
+        }
+    };
+
+    const addClickedClass = function (list) {
+        list.classList.add('clicked');
+    };
+
+    const handleNewClickedList = function (e) {
+        // Check if user clicks on an already clicked item
+        if (e.target.classList.contains('clicked')) return;
+
+        if (e.target.classList.contains('nav-item')) {
+            previousClickedList = newClickedList;
+            newClickedList = e.target;
+            removeClickedClass(previousClickedList);
+            addClickedClass(newClickedList);
+        }
+    };
+
+    navList.addEventListener('mousedown', handleNewClickedList);
+})();
